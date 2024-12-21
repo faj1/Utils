ccda3b544fc77e3e873017b32662e1e9f008e19d
www <222@qq.com>
2024-12-22 01:34:37 +0700

    新增文件工具

--- src/SnowflakeUtils.php
+++ src/SnowflakeUtils.php
@@ -4,16 +4,22 @@ namespace Faj1\Utils;
 
 use Godruoyi\Snowflake\RedisSequenceResolver;
 use Godruoyi\Snowflake\Snowflake;
+use Godruoyi\Snowflake\SnowflakeException;
 
 /**
  * 雪花ID工具
  */
 class SnowflakeUtils
 {
-
-
-
-    public static function GetId($redis = null,$datacenterId = 0, $workerId =1): string
+    /**
+     * 取雪花算法订单号
+     * @param $redis
+     * @param int $datacenterId
+     * @param int $workerId
+     * @return string
+     * @throws SnowflakeException
+     */
+    public static function GetId($redis = null, int $datacenterId = 0, int $workerId =1): string
     {
         $snowflake = new Snowflake($datacenterId, $workerId);
         $snowflake->setStartTimeStamp(strtotime('2024-09-09')*1000); // millisecond
@@ -28,6 +34,24 @@ class SnowflakeUtils
         $id = $snowflake->id();
         $redis->close();
         return $id;
+    }
+
+    /**
+     * 取指定长度的随机字符串
+     * @param int $length
+     * @return string
+     */
+    public static function generateRandomString(int $length = 10): string
+    {
+        // 定义允许的字符
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        // 循环生成随机字符串
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 
 
