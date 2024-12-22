ee152d1fbc5df897092d2c15164935db7f0ea2c5
www <222@qq.com>
2024-12-22 18:24:18 +0700

    新增文件工具

--- Test/SnowflakeUtilsTest.php
+++ Test/SnowflakeUtilsTest.php
@@ -2,6 +2,7 @@
 
 namespace Faj1\Utils\Test;
 
+use Faj1\Utils\database_pool\MyPDO;
 use Faj1\Utils\SnowflakeUtils;
 use Faj1\Utils\Socket\RevoltUnixServer;
 use Faj1\Utils\Socket\UnixClient;
@@ -11,15 +12,6 @@ use function Swoole\Coroutine\run;
 class SnowflakeUtilsTest extends TestCase
 {
 
-    public function hasDuplicates(array $arr): bool {
-        $valueCounts = array_count_values($arr);
-        foreach ($valueCounts as $value => $count) {
-            if ($count > 1) {
-                return true; // 存在重复值
-            }
-        }
-        return false; // 没有重复值
-    }
     public function testGetId()
     {
         $Data = [];
@@ -39,6 +31,15 @@ class SnowflakeUtilsTest extends TestCase
         $this->assertTrue(true, 'Code executed successfully without exceptions.');
     }
 
+    public function hasDuplicates(array $arr): bool {
+        $valueCounts = array_count_values($arr);
+        foreach ($valueCounts as $value => $count) {
+            if ($count > 1) {
+                return true; // 存在重复值
+            }
+        }
+        return false; // 没有重复值
+    }
 
     public function testSocket(){
 
@@ -54,7 +55,32 @@ class SnowflakeUtilsTest extends TestCase
     }
 
     public function testDbPool(){
+        $MyPDO = new MyPDO();
 
+        run(function () use (&$MyPDO) {
+               for ($i = 0; $i < 300; $i++) {
+                      go(function ()use ($MyPDO) {
+                          $Config = [
+                              'driver'      => 'mysql',
+                              'host'        => '127.0.0.1',
+                              'port'        => '3306',
+                              'database'    => 'tgbotxin',
+                              'username'    => 'tgbotxin',
+                              'password'    => 'MKMTGx4jeRai7t37',
+                              'charset'     => 'utf8mb4',
+                              'collation'   => 'utf8mb4_general_ci',
+                              'prefix'      => '',
+                              'strict'      => true,
+                              'engine'      => null,
+                          ];
+                          $Manager = $MyPDO->GetManager($Config);
+                          $user_center_invitation_code = $Manager->table('user_center_invitation_code')->where([])->first();
+                          var_dump($user_center_invitation_code);
+                      });
+               }
+        });
+
+         $this->assertTrue(true, 'Code executed successfully without exceptions.');
     }
 
 
